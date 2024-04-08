9e8ffa02ca19760246b3dbd16d1f9760ab09e350
Hackercat1826 <165159391+Hackercat1826@users.noreply.github.com>
2024-04-08 10:53:35 +0100

    Update Snakes.az.py

--- Snakes.az.py
+++ Snakes.az.py
@@ -1,4 +1,3 @@
-# importing libraries
 import pygame
 import time
 import random
@@ -9,7 +8,7 @@ snake_speed = 15
 window_x = 720
 window_y = 480
 
-# defining colors
+# defining colors with varibles, I always spell that wrong.
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
